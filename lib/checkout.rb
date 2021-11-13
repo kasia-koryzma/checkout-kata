d355ca89122c13426ae2048a69309972a17b96fe
Kasia koryzma <kasia.koryzma@hotmail.co.uk>
2021-11-13 13:00:55 +0000

    Update checkout.rb

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,43 +1,41 @@
+
+# Class for scanning and calculating the total price in the basket.
 class Checkout
   attr_reader :prices
   private :prices
 
-  def initialize(prices)
-    @prices = prices
+  def initialize(prices = nil)
+    @prices = prices || Prices.prices_hash
   end
 
   def scan(item)
+    # Only add items to basket if scan method returns successfully. Ignore unknown items.
+    raise InvalidItemError.new, "Unknown item #{item} scanned" if !@prices.include?(item)
     basket << item.to_sym
   end
 
   def total
     total = 0
-
-    basket.inject(Hash.new(0)) { |items, item| items[item] += 1; items }.each do |item, count|
-      if item == :apple || item == :pear
-        if (count % 2 == 0)
-          total += prices.fetch(item) * (count / 2)
-        else
-          total += prices.fetch(item) * count
-        end
-      elsif item == :banana || item == :pineapple
-        if item == :pineapple
-          total += (prices.fetch(item) / 2)
-          total += (prices.fetch(item)) * (count - 1)
-        else
-          total += (prices.fetch(item) / 2) * count
-        end
-      else
-        total += prices.fetch(item) * count
-      end
+    if !@prices || !@prices.is_a?(Hash)
+      raise UnknownPricesError.new, "Prices have not been defined"
     end
-
+    
+    basket.inject(Hash.new(0)) { |items, item| items[item] += 1; items }.each do |item, count|
+      pc = PriceCalculator.new(@prices)
+      total += pc.get_total_price_for_items(item, count)
+    end
     total
   end
 
   private
 
   def basket
-    @basket ||= Array.new
+    @basket ||= []
   end
+end
+
+class InvalidItemError < StandardError
+end
+
+class UnknownPricesError < StandardError
 end
